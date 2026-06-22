c2fe3dab2aa18a8988bd0559d700811849b551c8
kennedyk6955 <kennedyk6955@student.faytechcc.edu>
2026-06-22 07:32:15 -0400

    Add files via upload

---
+++ P1LAB1_KennedyKera.py
@@ -0,0 +1,9 @@
+# Kera Kennedy
+# 6/22/2026
+# P1LAB1
+# Write code that allows a person to enter in their name and produce a welcome message using the entered name
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello, " + first_name + " " + last_name + "! Welcome to CTI-110")
